c9570e4811782c0db76b072a52ce429dd7c234ab
John-S On System76 <git@suykerbuyk.org>
2025-12-04 19:32:38 -0700

    Fix tsh login config and integration.
    - Give up on using system clipboard.  Too many combinations of too
      many keyboard actions.  Just print the damned OTP.
    - Fix handling of the Teleport password to pure tmux, or not at all.
    - Fix management of teleport known hosts.

--- .local/bin/tsh-login-syketech.sh
+++ .local/bin/tsh-login-syketech.sh
@@ -21,7 +21,7 @@ tty)
     ;;
 esac
 
-echo "tsh login --proxy=syketech.com --user=suykerbuyk --ttl 1800"
+echo "tsh login --proxy=$PROXY --user=$USER --ttl 1800"
 
 if [ "X${TMUX}X" != "XX" ]; then
     tmux set-buffer "$(op item get "Syketech Teleport" --fields label=password --reveal)"
@@ -30,21 +30,9 @@ else
     echo "Not running in tmux, you will need to enter the Teleport password manually"
 fi
 
-which wl-copy >/dev/null
-if [ $(which xsel) ]; then
-    echo "$OTP" | xsel --primary
-    echo "One time password is now in primary clipboard: $OTP"
-elif [ $(which wl-copy) ]; then
-    echo "$OTP" | wl-copy
-    echo "One time password is now in primary clipboard: $OTP"
-else
-    echo "No system clipboard functions detected."
-    echo "OTP: $OTP"
-fi
-echo "tsh login --proxy=${PROXY} --user=${USER} --ttl 1800"
+echo "One time password: $OTP"
 tsh login --proxy=${PROXY} --user=${USER} --ttl 1800
 
-echo "" | wl-copy
 if [ "X${TMUX}X" != "XX" ]; then
     tmux set-buffer ""
 fi
