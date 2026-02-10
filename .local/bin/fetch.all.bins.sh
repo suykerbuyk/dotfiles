c5d38efcb04e2ee5a284756a1b84c4e17324380d
John Suykerbuyk <jsuykerbuyk@mettleops.com>
2026-02-09 19:11:44 -0700

    Fix up relative path issue in fetch bins

--- .local/bin/fetch.all.bins.sh
+++ .local/bin/fetch.all.bins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR="$(dirname $(realpath update.local.bins.sh))"
 for F in $(find ${SCRIPT_DIR}/fetch.bins/ -type f -executable | sort); do
     echo $F
     $F
