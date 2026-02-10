207b3ff997ab3e0e12aaf8bd065e6b4f19829312
John-S On System76 <git@suykerbuyk.org>
2026-02-10 02:08:10 -0700

    Fix fetch bins

--- .local/bin/fetch.all.bins.sh
+++ .local/bin/fetch.all.bins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DIR="$(dirname $(realpath update.local.bins.sh))"
+SCRIPT_DIR="$(dirname $(realpath $0))"
 for F in $(find ${SCRIPT_DIR}/fetch.bins/ -type f -executable | sort); do
     echo $F
     $F
